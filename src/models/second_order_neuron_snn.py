867c3453c46acfc4ec8d415b1a539875bc2d6710
Johann Aschenloher <hansi.aschenloher@gmail.com>
2024-01-09 10:48:33 +0100

    Add a simple model with a 2. order neuron

---
+++ src/models/second_order_neuron_snn.py
@@ -0,0 +1,35 @@
+import snntorch as snn
+import torch
+import torch.nn as nn
+
+
+class SimpleFC(nn.Module):
+    def __init__(self, alpha, beta, num_steps, **kwargs):
+        super().__init__()
+        self.num_steps = num_steps
+        num_inputs = 28 * 28
+        num_hidden = 100
+        num_outputs = 10
+
+        # Initialize layers
+        self.fc1 = nn.Linear(num_inputs, num_hidden)
+        self.lif1 = snn.Synaptic(alpha=alpha, beta=beta)
+
+        self.fc2 = nn.Linear(num_hidden, num_outputs)
+        self.lif2 = snn.Synaptic(alpha=alpha, beta=beta)
+
+    def forward(self, x):
+        # Initialize hidden states at t=0
+        syn1, mem1 = self.lif1.init_synaptic()
+        syn2, mem2 = self.lif2.init_synaptic()
+        spk2_rec = []
+        mem2_rec = []
+        for step in range(self.num_steps):
+            cur1 = self.fc1(x[:,step])
+            spk1, syn1, mem1 = self.lif1(cur1, syn1, mem1)
+            cur2 = self.fc2(spk1)
+            spk2, syn2, mem2 = self.lif2(cur2, syn2, mem2)
+            spk2_rec.append(spk2)
+            mem2_rec.append(mem2)
+
+        return torch.stack(spk2_rec, dim=0), torch.stack(mem2_rec, dim=0)
