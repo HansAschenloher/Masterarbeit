b21c6a6f147b6ccac5330f5259726299901fb1d5
Johann Aschenloher <hansi.aschenloher@gmail.com>
2024-04-30 00:51:19 +0200

    Add BNTT experiment

--- src/models/bntt.py
+++ src/models/bntt.py
@@ -1,43 +1,30 @@
-from typing import cast, List, Union, Callable, Tuple, Optional, Any
+from typing import cast, List, Union, Tuple, Any
 
 import snntorch as snn
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from snntorch import utils
 
-
-import sys
-sys.path.append('..')
-
-
 configurations = {
-
-    'BNTT': [
-        [64, 64, 'A'],
-        [128, 128, 'A'],
-        [256, 256, 256, 'A'],
-    ],
-
     'VGG7': [
         [64, 'M'],
         [128, 'M'],
         [256, 'M'],
         [512, 'M']
     ],
-
     'VGG9': [
         [64, 'M'],
         [128, 'M'],
         [256, 256, 'M'],
         [512, 512, 'M']
     ],
-
     'VGG11': [
-        [64, 'M'],
-        [128, 'M'],
-        [256, 256, 'M'],
-        [512, 512, 'M'],
-        [512, 512, 'M']
+        [64, 'A'],
+        [128, 'A'],
+        [256, 256, 'A'],
+        [512, 512, 'A'],
+        [512, 512]
     ],
     'VGG13': [
         [64, 64, 'M'],
@@ -70,17 +57,17 @@ def make_layers(cfg: List[Union[str, int]], batch_norm: bool = False, num_steps=
     for layer in cfg:
         for value in layer:
             if value == "M":
-                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-            if value == "A":
-                layers += [nn.AvgPool2d(kernel_size=2, stride=2)]
+                layers += [nn.MaxPool2d(kernel_size=2)]
+            elif value == "A":
+                layers += [nn.AvgPool2d(kernel_size=2)]
             else:
                 value = cast(int, value)
-                conv2d = nn.Conv2d(in_channels, value, kernel_size=3, padding=1)
+                conv2d = nn.Conv2d(in_channels, value, kernel_size=3, padding=1, stride=1, bias=False)
                 if batch_norm:
                     layers += [conv2d,
                                BatchNorm2dThroughTime(num_steps=num_steps, num_features=value, eps=1e-4, momentum=0.1,
                                                       affine=affine_flag),
-                               snn.Leaky(beta=beta, init_hidden=True)]
+                               snn.Leaky(beta=beta, init_hidden=True, spike_grad=Surrogate_BP_Function.apply)]
                 else:
                     layers += [conv2d, snn.Leaky(beta=beta, init_hidden=True)]
                 in_channels = value
@@ -157,55 +144,67 @@ class BatchNorm1dThroughTime(TemporalLayer):
         super().__init__(module, num_steps)
 
 
+class Surrogate_BP_Function(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        out = torch.zeros_like(input).cuda()
+        out[input > 0] = 1.0
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad = grad_input * 0.3 * F.threshold(1.0 - torch.abs(input), 0, 0)
+        return grad
+
+
 class VGG(nn.Module):
-    def __init__(self, features: nn.Module, num_classes: int = 10, num_steps=64):
+    def __init__(self, features: nn.Module, num_classes: int = 10, num_steps=64, beta=0.95):
         super().__init__()
         self.num_steps = num_steps
-        self.beta = 0.9
+        self.num_classes = num_classes
+        self.beta = beta
         self.features = features
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        classifier_hidden_layer = 4096
         self.classifier = TemporalSequential(num_steps,
-                                             nn.Linear(256 * 4 * 4, 1024),
-                                             BatchNorm1dThroughTime(self.num_steps, 1024, eps=1e-4, momentum=0.1,
+                                             nn.Linear(512, classifier_hidden_layer, bias=False),
+                                             BatchNorm1dThroughTime(self.num_steps, classifier_hidden_layer, eps=1e-4,
+                                                                    momentum=0.1,
                                                                     affine=True),
-                                             snn.Leaky(beta=self.beta, init_hidden=True),
-                                             nn.Linear(1024, num_classes),
-                                             snn.Leaky(beta=self.beta, init_hidden=True, output=True)
+                                             snn.Leaky(beta=self.beta, init_hidden=True,
+                                                       spike_grad=Surrogate_BP_Function.apply),
+                                             nn.Linear(classifier_hidden_layer, num_classes, bias=False),
                                              )
+        # Init weights
+        for m in self.modules():
+            if (isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d)):
+                m.bias = None
+            elif (isinstance(m, nn.Conv2d)):
+                torch.nn.init.xavier_uniform_(m.weight, gain=2)
+            elif (isinstance(m, nn.Linear)):
+                torch.nn.init.xavier_uniform_(m.weight, gain=2)
 
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        #utils.reset(self.features)
-        #utils.reset(self.classifier)
-        mem_rec1 = [];
-        spk_rec1 = [];
+        utils.reset(self.features)
+        utils.reset(self.classifier)
+        mem_fc2 = torch.zeros(self.num_classes, device=torch.get_device(x))
         for t in range(self.num_steps):
             spk_x = self.features(x[:, t], t)
-            # spk_x = self.avgpool(spk_x)
+            spk_x = self.avgpool(spk_x)
             spk_x = torch.flatten(spk_x, 1)
-            spk_x, mem_x = self.classifier(spk_x, t)
-            mem_rec1.append(mem_x)
-            spk_rec1.append(spk_x)
+            out = self.classifier(spk_x, t)
+            mem_fc2 = mem_fc2 + out
 
-        return torch.stack(spk_rec1, dim=0), torch.stack(mem_rec1, dim=0)
+        return mem_fc2 / self.num_steps
 
 
-def _vgg(cfg: str, batch_norm: bool, progress: bool, num_steps, num_cls, **kwargs: Any) -> VGG:
-    model = VGG(make_layers(configurations[cfg], batch_norm=batch_norm, num_steps=num_steps), num_steps=num_steps, num_classes=num_cls,
-                **kwargs)
+def vgg_with_bntt(cfg: str, batch_norm: bool, num_steps, num_cls, beta=0.95, **kwargs: Any) -> VGG:
+    model = VGG(make_layers(configurations[cfg], batch_norm=batch_norm, num_steps=num_steps, beta=beta),
+                num_steps=num_steps,
+                num_classes=num_cls,
+                beta=beta, **kwargs)
     return model
-
-
-def vgg7(num_steps=64) -> VGG:
-    return _vgg("VGG7", False, False, num_steps=num_steps)
-
-
-def vgg9(num_steps=64) -> VGG:
-    return _vgg("VGG9", False, False, num_steps=num_steps)
-
-
-def vgg11(num_steps=64, batch_norm=False, num_cls=100) -> VGG:
-    return _vgg("VGG11", batch_norm, False, num_steps=num_steps, num_cls=num_cls)
-
-
-def bntt(num_steps=25, num_cls=100, batch_norm=True) -> VGG:
-    return _vgg('BNTT', batch_norm, False, num_steps, num_cls)
